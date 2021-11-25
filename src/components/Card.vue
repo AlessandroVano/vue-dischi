d301f7b13f977e29e4c6ba9ba858c6dec68ee82d
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-26 00:55:47 +0100

    insert component card + promps in CardMusic + inizio css di modifiche

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sist">
+        <img :src="image" :alt="title">
+     <h3> {{title}} </h3>
+     <h4> {{subTitle}} </h4>
+     <h5> {{text}} </h5>
+    <span>{{subText}} </span>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Card',
+props: {
+    image: String,
+    title: String,
+    subTitle: String,
+    text: String,
+    subText: String,
+}
+}
+</script>
+
+<style  scoped lang="scss">
+@import '@/styles/globals';
+
+img {
+    width: 199px; 
+    display: flex;
+    justify-content: space-between;
+    padding: 23px;
+    margin-top: 202px;
+  }
+ .sist {
+   background: $bg-color;
+ }
+</style>
--- src/components/CardMusic.vue
+++ src/components/CardMusic.vue
@@ -2,18 +2,38 @@
 <div class="container">
   <div class="row">
 
+   <div class="col-2"
+    v-for="(cdMusic, index) in CardMusic " 
+    :key="`cdMusic-${index}`"
+    >
+    
+      <Card 
+        :image="cdMusic.poster"
+        :title="cdMusic.title"
+        :subTitle="cdMusic.author"
+        :text="cdMusic.genre"
+        :subText="cdMusic.year"
+      />
+   </div>
   </div>
 </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Card from '@/components/Card.vue';
 
 export default {
 name: 'CardMusic',
+
+components: {
+  Card,
+
+},
+
 data() {
     return {
-         CardMusic: [],
+         CardMusic: null,
         };
   },
   created() {
@@ -24,7 +44,8 @@ data() {
         
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
         .then(result =>{
-            console.log(result.data);
+             console.log(result.data); 
+            this.CardMusic = result.data.response
         })
         .catch(err => console.log(err));
       }
