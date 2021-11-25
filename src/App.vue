92710a844c1a3a21c205c76b787d07fd9535bf91
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-25 17:07:18 +0100

    console.log cardMusic

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
 <div id="app">
   <Header />
+
+   <CardMusic />
 </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import CardMusic from '@/components/CardMusic.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    CardMusic,
   }
 }
 </script>
---
+++ src/components/CardMusic.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="container">
+  <div class="row">
+
+  </div>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+name: 'CardMusic',
+data() {
+    return {
+         CardMusic: [],
+        };
+  },
+  created() {
+     this.getCreateCardMusic();
+  },
+  methods: {
+      getCreateCardMusic() {
+        
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then(result =>{
+            console.log(result.data);
+        })
+        .catch(err => console.log(err));
+      }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
